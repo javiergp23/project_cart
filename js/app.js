e01c4722b10ff2fb324ec5a809d9f318bebef49e
Javier Gonzalez <javgon2306@gmail.com>
2024-08-03 00:44:23 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const listaCarrito = document.querySelector('lista-carrito');
+const carrito = document.querySelector('carrito');
+const vaciarCarrito = document.querySelector('vaciar-carrito');
+
+function agregarProducto(listaCarrito) {
+    listaCarrito.addEventListener('click', listaCarrito)
+    console.log('carrito datos')
+}
